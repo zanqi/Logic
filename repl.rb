5d855cc0d497c48464dbb37f5420a06381b6575e
Zanqi Liang <zanqi.liang@gmail.com>
2013-06-20 21:29:43 -0400

    Simple query

--- repl.rb
+++ repl.rb
@@ -42,11 +42,11 @@ def query_syntax_process(exp)
 end
 
 def map_over_symbols(proc, exp)
-    if exp.kind_of?(Array)
+    if exp.is_a?(Array)
         exp.map do |e|
             map_over_symbols proc, e
         end
-    elsif exp.kind_of?(Symbol)
+    elsif exp.is_a?(Symbol)
         proc.(exp)
     else
         exp
@@ -61,7 +61,7 @@ def instantiate(exp, frame, unbound_var_handler)
             else
                 unbound_var_handler exp1 frame
             end
-        elsif exp1.kind_of?(Array)
+        elsif exp1.is_a?(Array)
             exp1.map(&copy)
         else
             exp1
@@ -80,13 +80,12 @@ end
 
 def add_rule! rule
     @all_rules << rule
-    if indexable? rule
-        @indexed_rules[index_key_of rule] = @indexed_rules[index_key_of rule] << rule
+    if indexable? rule[1]
+        @indexed_rules[index_key_of rule[1]] = @indexed_rules[index_key_of rule[1]] << rule
     end
 end
 
 def add_assertion! assertion
-    p assertion
     @all_assertions << assertion
     if indexable? assertion
         @indexed_assertions[index_key_of assertion] = @indexed_assertions[index_key_of assertion] << assertion
@@ -94,7 +93,7 @@ def add_assertion! assertion
 end
 
 def indexable? pattern
-    pattern[0].kind_of?(Symbol) || var?(pattern)
+    pattern[0].is_a?(Symbol) || var?(pattern[0])
 end
 
 def index_key_of pat
@@ -102,7 +101,7 @@ def index_key_of pat
 end
 
 def var? item
-    item.kind_of?(Array) && item[0] == "?"
+    item.is_a?(Array) && item[0] == "?"
 end
 
 def assertion_to_be_added?(exp)
@@ -114,8 +113,8 @@ def assertion_body exp
 end
 
 def qeval(query, frames)
-    if built_in.has_key? query[0]
-        built_in[query[0]].(query.drop(1), frames)
+    if @built_in.has_key? query[0]
+        @built_in[query[0]].(query.drop(1), frames)
     else
         simple_query query, frames
     end
@@ -123,7 +122,122 @@ end
 
 def simple_query query, frames
     frames.flat_map do |frame|
-        find_assertions(query, frame) + find_rules(query, frame) 
+        apply_assertions(query, frame) + apply_rules(query, frame) 
+    end
+end
+
+def apply_rules pattern, frame
+    fetch_rules(pattern, frame).flat_map do |rule|
+        apply_a_rule rule, pattern, frame
+    end
+end
+
+def apply_a_rule rule, pattern, frame
+    clean_rule = rename rule
+    result = unify_match pattern, clean_rule[1], frame
+    result == 'failed' ? [] : [result]
+end
+
+def unify_match p1, p2, frame
+    if frame == 'failed'
+        'failed'
+    elsif p1 == p2
+        frame
+    elsif var? p1
+        extend_if_possible p1, p2, frame
+    elsif var? p2
+        extend_if_possible p2, p1, frame
+    elsif p1.is_a?(Array) && p2.is_a?(Array)
+        unify_match p1.drop(1), p2.drop(1), unify_match(p1[0], p2[0], frame)
+    else
+        'failed'
+    end
+end
+
+def extend_if_possible var, val, frame
+    if frame.has_key? var
+        unify_match frame[var], val, frame
+    elsif var? val
+        if frame.has_key? val
+            unify_match var, frame[val], frame
+        else
+            frame[var] = val
+        end
+    elsif depends_on? val, var, frame
+        'failed'
+    else
+        frame[var] = val
+    end
+end
+
+def depends_on? exp, var, frame
+    if var? exp
+        if var == exp
+            true
+        else
+            frame.has_key?(exp) ? depends_on?(frame[exp], var, frame) : false
+        end
+    elsif exp.is_a?(Array)
+        depends_on?(exp[0], var, frame) || depends_on?(exp.drop(1), var, frame)
+    else
+        false
+    end
+end
+
+def rename exp
+    if var? exp
+        
+    elsif exp.is_a?(Array)
+        exp.map { |e| rename e }
+    else
+        exp
+    end
+end
+
+def apply_assertions pattern, frame
+    fetch_assertions(pattern, frame).flat_map do |datum|
+        check_an_assertion datum, pattern, frame
+    end
+end
+
+def fetch_assertions pattern, frame
+    use_index?(pattern) ? @indexed_assertions[index_key_of pattern] : @all_assertions
+end
+
+def fetch_rules pattern, frame
+    use_index?(pattern) ? @indexed_rules[index_key_of pattern] : @all_rules
+end
+
+def use_index? pat
+    pat[0].is_a?(Symbol) || var?(pat[0])
+end
+
+def check_an_assertion assertion, query_pat, frame
+    result = pattern_match query_pat, assertion, frame
+    p result
+    result == "failed" ? [] : [result]
+end
+
+def pattern_match pat, dat, frame
+    if frame == "failed"
+        "failed"
+    elsif pat == dat
+        frame
+    elsif var? pat
+        extend_if_consistent pat, dat, frame
+    elsif pat.is_a?(Array) && dat.is_a?(Array)
+        pattern_match pat.drop(1), dat.drop(1), pattern_match(pat[0], dat[0], frame)
+    else
+        "failed"
+    end
+end
+
+def extend_if_consistent var, dat, frame
+    if frame.has_key?(var)
+        pattern_match frame[var], dat, frame
+    else
+        frame[var] = dat
+        frame
     end
 end
 
@@ -141,9 +255,7 @@ def repl
         input = Readline.readline("input> ", true)
         if input == 'reload'
             load 'repl.rb'
-            puts "done"
-            repl
-            return        
+            next
         end
         break if input =~ /^exit$/i
 
@@ -154,12 +266,10 @@ def repl
 
             if assertion_to_be_added? q
                 add_rule_or_assertion! assertion_body(q)
-                puts
                 puts "Assertion added to data base."
 
-                p @all_assertions, @all_rules, @indexed_assertions, @indexed_rules
+                # p @all_assertions, @all_rules, @indexed_assertions, @indexed_rules
             else
-                puts
                 p qeval(q, [{}])
             end
 
@@ -171,6 +281,16 @@ def repl
             # Clear ANSI escapes
             print "\e[0m"
         end
+    end
+end
+
+def print_result frames
+    if frames == []
+        puts "No"
+    elsif frames = [{}]
+        puts "Yes"
+    else
+        frames.each { |f| puts f }
     end
 end
 
